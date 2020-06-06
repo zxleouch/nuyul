78f1c7fa3159db2f857cb9b6c8581049ce735730
km37id <65444913+km37id@users.noreply.github.com>
2020-06-06 19:23:39 +0700

    Add files via upload

---
+++ data.php
@@ -0,0 +1,7 @@
+<?php 
+
+$ua = 'user-agent:Mozilla/5.0 (Linux; U; Android 8.1.0; en-us; CPH1909 Build/O11019) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30';
+$bearer = 'Authorization:Bearer eyJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE1OTE0MzgwNjQsInN1YiI6IjI5MTk1OTY4MzQ3NTYyNzgiLCJleHAiOjE1OTE3MzgwNjR9.g3zoWybccJ6JJBC9AHiU2bsFzWV0M6Hfq8cLwKtbIE0cAQEEv7U5tMDtyl7C_oui8Vett2-VLyGlWAMaSDyjjgv2r_JwSb17cFv-Xo1YHqLqI3BzfyakKEh2fWWYFY9KSI-M5DBYbM-N1Qc6IcwgmFCZ9Fs5azpQxcF2vTiO_27IjEsVbcnWQ7uODbeppRGozSw7S8aCRFoX1J4DKH_3Dkc8J7rfaORDtxh-H3r29BSmttAhSXuZnPFIPRAdu8CZiioblACgfTSjT9J-HdawJC-PaCGP3tnntSOszOqfeJ3YGakjeRO3GEaXFKMlDtCmZi2K18V8sUu_UbJrCuAasg';
+
+
+ ?>
